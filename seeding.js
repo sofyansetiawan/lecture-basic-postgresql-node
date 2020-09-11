ca8735472fd381a04f87ae15f16954029b41b003
Sofyan Setiawan <sofyansetiawannn@gmail.com>
2020-09-11 11:24:53 +0700

    Update seeding.js

--- seeding.js
+++ seeding.js
@@ -4,6 +4,8 @@ const client = require("./config");
 
 const data = fs.readFileSync("./person.csv", "utf-8").split("\n").splice(1);
 
+// * SEEDING BERIKUT INI MEMBUAT DATA TIDAK TERURUT. GUNAKAN SEEDING DENGAN LOOPING BIASA UNTUK MEMBENTUK PERINTAH QUERY INSERT BESERTA DATANYA
+
 for(let i = 0; i < data.length; i++){
     let singleData = data[i].split(",");
 
